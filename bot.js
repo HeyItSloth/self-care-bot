d5af3a1f9e667cf8d1b3b489a30b60febec7b889
George <heyitsloth@outlook.com>
2022-05-15 15:15:05 +0100

    Base files

---
+++ bot.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js');
+const { token } = require('./config.json');
+const { Sequelize } = require('sequelize');
+const chalk = require('chalk');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    console.log(chalk.yellow(`>> Initializing ${file}...`));
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+console.log(chalk.green(`> Completed command init.`))
+
+client.buttons = new Collection();
+const buttonFiles = fs.readdirSync(`./buttons`).filter(file => file.endsWith('.js'));
+
+for (const file of buttonFiles) {
+    console.log(chalk.yellow(`>> Initializing ${file}...`));
+    const button = require(`./buttons/${file}`);
+    client.buttons.set(button.data, button);
+}
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host:       'localhost',
+    dialect:    'sqlite',
+    logging:    'false',
+    storage:    'database.sqlite'
+});
+
+client.once('ready', () => {
+    client.user.setActivity({
+        name: "my friends be healthy | ❤️",
+        type: "WATCHING"
+    });
+
+    const d = new Date();
+    let init = Date.now();
+    console.log(chalk.blueBright(`> Bot ready, initialization complete at ${d.toLocaleString(init)}`));
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+    
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply(`Something went wrong!`);
+    };
+});
+
+client.login(token);
